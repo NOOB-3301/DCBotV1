9724120c08ba4e6cf1eba9dc7f6ba54d0a39a877
NOOB-3301 <sepic466@gmail.com>
2025-04-16 00:18:09 +0530

    Implement basic bot functionality with ping command and client setup

--- Bot/app.js
+++ Bot/app.js
@@ -8,3 +8,13 @@ if (!token) {
     console.log("no token found")
     process.exit(1)
 }
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.DirectMessages] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
